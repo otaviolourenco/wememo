bdf5cb81c0b39e2bb4eaa0c4d91df28f445b4e01
Otávio Lourenço <otavioluissv@gmail.com>
2024-08-25 11:59:40 +0100

    Initial commit

---
+++ wememo/wememoApp.swift
@@ -0,0 +1,17 @@
+//
+//  wememoApp.swift
+//  wememo
+//
+//  Created by Otavio Lourenço on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct wememoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
