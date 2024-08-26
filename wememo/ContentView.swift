0789ad426a5da4f930caa66f02f9c5e9a8e73e5b
Otávio Lourenço <otavioluissv@gmail.com>
2024-08-26 22:53:02 +0100

    Togglable isFaceUp

--- wememo/ContentView.swift
+++ wememo/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
+            CardView()
             CardView()
             CardView()
             CardView()
@@ -21,19 +21,23 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            
+            let base = RoundedRectangle(cornerRadius: 16)
+            
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 16)
-                    .strokeBorder(lineWidth: 2 )
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2 )
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 16)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
